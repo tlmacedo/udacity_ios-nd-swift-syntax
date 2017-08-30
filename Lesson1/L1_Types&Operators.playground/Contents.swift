5f6105ca19749ef212e20cbbc5cca1df9ed40241
thiagomacedo <tl.macedo@hotmail.com>
2017-08-30 00:02:14 -0400

    auto commit

--- Lesson1/L1_Types&Operators.playground/Contents.swift
+++ Lesson1/L1_Types&Operators.playground/Contents.swift
@@ -1,5 +1,53 @@
-//: Playground - noun: a place where people can play
-
+//: # Types
 import UIKit
+import Foundation
+//: ### Example 1: Bool, Int, Float, Double
+class LightSwitch {
+    var on = true
+    var dimmer = 3.14159265359
 
-var str = "Hello, playground"
+}
+
+var livingRoomSwitch = LightSwitch()
+livingRoomSwitch.on
+//: ### Example 2; Strings and Characteres
+var dollarSign = "$"
+var myFirstSwiftString = "mo' money"
+var mySecondSwiftString = "mo' problems"
+var concatnatedString = myFirstSwiftString + ", " + mySecondSwiftString
+type(of: concatnatedString)
+
+//: ### Stay tuned for more on Optionals and Tuples in the upcoming lessons!
+
+//: # Operators
+
+//: ### Example 1 - Comparison operators
+let ticketPrice = 7.5
+let allowance = 10.0
+var iceCreamPrice = 2.5
+
+var pic = #imageLiteral(resourceName: "Chloe.png")
+
+if allowance >= ticketPrice + iceCreamPrice {
+    print("Let's go to the movies!")
+} else {
+    print("Let's watch a movie at home and eat ice cream")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
